c829a6e7fb5f5515bd6976705841785a88e23bf8
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-02-16 14:10:56 +1300

    send get request

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.get('/', (req, res) => {
+  console.log(req.method)
+  res.sendStatus(200)
+})
+
 app.listen(PORT, () => {
   console.log(`Server has started on: ${PORT}`)
 })
