30c9521e24c63ef643d744b9b91796b71c43cf89
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-02-17 16:00:30 +1300

    stringify json data to test homepage

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -2,13 +2,27 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+let data = {
+  name: 'Bean',
+}
+
+// website endpoints
 app.get('/', (req, res) => {
-  res.send('<h1>This is html code</h1><input></input>')
+  res.send(`
+    <body>
+    <p>${JSON.stringify(data)}</p>
+    </body>
+    `)
 })
 
 app.get('/dashboard', (req, res) => {
-  console.log('Hit /dashboard endpoint')
-  res.send('HELLO')
+  res.send('<h1>DAshboard</h1>')
+})
+
+// API endpoints
+app.get('/api/data', (req, res) => {
+  console.log('this one is for data')
+  res.send(data)
 })
 
 app.listen(PORT, () => {
