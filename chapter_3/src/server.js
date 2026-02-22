6a1d10634c0f2f9f45cd5d51ebf6382f958611e7
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-02-23 09:54:03 +1300

    complete initial setup of auth routes and begin todo routes

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
 
 //Routes
 app.use('/auth', authRoutes)
+app.use('/todo', todoRoutes)
 
 //Port
 app.listen(PORT, () => {
