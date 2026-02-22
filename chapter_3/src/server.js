091d707075931121ddfa4a7bf3217a988e100ca9
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-02-23 10:32:45 +1300

    add jwt secret to env

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -5,7 +5,7 @@ import authRoutes from './routes/authRoutes.js'
 import todoRoutes from './routes/todoRoutes.js'
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5003
 
 // Filepath
 const __filename = fileURLToPath(import.meta.url)
