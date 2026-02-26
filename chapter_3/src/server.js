66ffb03d6be4c3ad31a017f25e32d965d9c55744
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-02-26 16:59:43 +1300

    import middleware and add to todo route

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -3,6 +3,7 @@ import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import authRoutes from './routes/authRoutes.js'
 import todoRoutes from './routes/todoRoutes.js'
+import authMiddleware from './middleware/authMiddleware.js'
 
 const app = express()
 const PORT = process.env.PORT || 5003
@@ -21,7 +22,7 @@ app.get('/', (req, res) => {
 
 //Routes
 app.use('/auth', authRoutes)
-app.use('/todos', todoRoutes)
+app.use('/todos', authMiddleware, todoRoutes)
 
 //Port
 app.listen(PORT, () => {
